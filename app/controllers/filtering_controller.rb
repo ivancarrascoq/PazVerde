b74c48acbec169d705fb290a6a739768d788e7db
ivancarrascoq <ivancarrascoq@gmail.com>
2018-08-31 18:34:37 -0500

    new view inherit from spree

--- app/controllers/filtering_controller.rb
+++ app/controllers/filtering_controller.rb
@@ -1,12 +1,23 @@
-class FilteringController < ApplicationController
-#Spree::HomeController#index
+#class FilteringController < ApplicationController
+class FilteringController < Spree::StoreController
+   helper 'spree/products'
+   respond_to :html
 #  layout 'spree/layouts/spree_application'
   def filter
+    @searcher = build_searcher(params.merge(include_images: true))
+    @products = @searcher.retrieve_products
+    @products = @products.includes(:possible_promotions) if @products.respond_to?(:includes)
+    @taxonomies = Spree::Taxonomy.includes(root: :children)
   end
   def index
-    @title = "ComparaGrow"
+#    Spree::Config[:layout]='layouts/application'
+#    render :partial => '/spree/shared/head', :locals => {title: "ComparaGrow"}
     @id1 = params[:id1]
     @id2 = params[:id2]
+    @searcher = build_searcher(params.merge(include_images: true))
+    @products = @searcher.retrieve_products
+    @products = @products.includes(:possible_promotions) if @products.respond_to?(:includes)
+    @taxonomies = Spree::Taxonomy.includes(root: :children)
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
 #  class filteringController < ApplicationController
 #    layout '/spree/layouts'
 #  end
-  get 'filtering/filter'
+#  Spree::Core::Engine.routes.prepend do
+  get 'filtering/filter', to: 'filtering#filter'
+##commented  get 'filtering/index/:id1/:id2', to: 'filtering#index', as: 'filtering' 
   get 'filtering/index/:id1/:id2', to: 'filtering#index', as: 'filtering' 
-
+#  end
   # This line mounts Spree's routes at the root of your application.
   # This means, any requests to URLs such as /products, will go to
   # Spree::ProductsController.
@@ -15,4 +17,6 @@ Rails.application.routes.draw do
   # the default of "spree".
   mount Spree::Core::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#  get 'filtering/index/:id1/:id2', to: 'filtering#index', as: 'filtering' 
+
 end
